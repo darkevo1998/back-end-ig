39c70d87b0bb03fde44816620b6b143deb2542a4
darkevo24 <timotius_albert@yahoo.com>
2025-04-10 16:54:55 +0700

    update passport

--- config/passport.js
+++ config/passport.js
@@ -1,42 +1,31 @@
-module.exports = function(passport) {
-  const InstagramStrategy = require('passport-instagram').Strategy;
-  const User = require('../models/User');
+const InstagramStrategy = require('passport-oauth2').Strategy;
 
-  passport.use(new InstagramStrategy({
+passport.use(new InstagramStrategy({
+    authorizationURL: 'https://api.instagram.com/oauth/authorize',
+    tokenURL: 'https://api.instagram.com/oauth/access_token',
     clientID: process.env.INSTAGRAM_CLIENT_ID,
     clientSecret: process.env.INSTAGRAM_CLIENT_SECRET,
     callbackURL: process.env.INSTAGRAM_CALLBACK_URL,
+    scope: ['user_profile', 'user_media'],
+    state: true,
     passReqToCallback: true
   },
   async (req, accessToken, refreshToken, profile, done) => {
     try {
-      let user = await User.findOne({ instagramId: profile.id });
-      
-      if (!user) {
-        user = new User({
-          instagramId: profile.id,
+      const userId = await getInstagramUserId(accessToken); // Same as before
+      let user = await User.findOneAndUpdate(
+        { instagramId: userId },
+        { 
           username: profile.username,
-          accessToken: accessToken
-        });
-        await user.save();
-      }
-      
+          accessToken: accessToken,
+          lastLogin: new Date()
+        },
+        { upsert: true, new: true }
+      );
       return done(null, user);
     } catch (err) {
+      console.error('Auth error:', err);
       return done(err);
     }
-  }));
-
-  passport.serializeUser((user, done) => {
-    done(null, user.id);
-  });
-
-  passport.deserializeUser(async (id, done) => {
-    try {
-      const user = await User.findById(id);
-      done(null, user);
-    } catch (err) {
-      done(err);
-    }
-  });
-};
+  }
+));
